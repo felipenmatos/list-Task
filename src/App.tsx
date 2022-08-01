734368b5e9088832e18d50036c99a512765defdd
Felipe Nonato Carvalho Matos <felipe.matos@awscode.com.br>
2022-08-01 14:58:28 -0300

    feat: partial completion of the project

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,28 @@ import { useState } from "react";
 import { Container, Area, Header } from "./App.styles";
 import { Item } from "./types/item";
 import { ListItem } from "./components/Listitem";
+import { AddArea } from "./components/AddArea";
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
     { id: 1, name: "Comprar pão", done: false },
     { id: 2, name: "Estudar", done: false },
   ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false,
+    });
+    setList(newList);
+  };
   return (
     <Container>
       <Area>
         <Header>Lista de Tarefas</Header>
+        <AddArea onEnter={handleAddTask} />
         {list.map((item, index) => (
           <ListItem key={index} item={item} />
         ))}
